51b9687f3787a086936f0698dd6ee0450f58aa91
zachary obeid <zobeid@csub.edu>
2017-04-22 20:45:57 -0700

    Moved Framework Code

--- robertP.h
+++ robertP.h
@@ -7,7 +7,6 @@ void loadSound(const char path[20]);
 void playSound(float pitch, bool loop);
 void endSound();
 void closeSoundDevice();
-void loadImages();
 //void deathAnimation(int x, int y);
 void deathPhysics();
 void deathRender();
